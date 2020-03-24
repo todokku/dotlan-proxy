7bd3fd7a1b848a733f0ed30e00235d1f3c8d4ddf
NightCandle <instkffff@gmail.com>
2020-03-24 23:35:55 +0800

    .

---
+++ mproxy.js
@@ -0,0 +1,49 @@
+const http = require('http')
+const https = require('https')
+const httpProxy = require('http-proxy')
+const url = require('url')
+const connect = require('connect')
+const harmon = require('harmon')
+
+selects = []
+simpleselect = {}
+simpleselect1 = {}
+
+simpleselect.query = '[src*="jquery.min"]'
+simpleselect1.query = '[src*="jquery-ui.min"]'
+
+
+simpleselect.func = function(node) {
+	node.setAttribute('src', 'http://libs.baidu.com/jquery/1.5.1/jquery.min.js')
+}
+simpleselect1.func = function(node) {
+	node.setAttribute('src', 'http://libs.baidu.com/jqueryui/1.8.10/jquery-ui.min.js')
+}
+
+selects.push(simpleselect)
+selects.push(simpleselect1)
+
+
+finalUrl = 'https://evemaps.dotlan.net/'
+parsedUrl = url.parse(finalUrl)
+
+proxy = httpProxy.createProxyServer({
+	target: finalUrl,
+	agent: https.globalAgent,
+	headers: {
+		host: parsedUrl.hostname
+	},
+	prependPath: false,
+	hostRewrite: finalUrl.host,
+	protocolRewrite: parsedUrl.protocol,
+	xfwd: true
+})
+
+const app = connect()
+
+app.use(harmon([],selects))
+app.use(function(req,res){
+	proxy.web(req,res)
+})
+
+http.createServer(app).listen(8000)
